9f90bc4ddfe186e78cc4f40600fbe58960f198f3
Bedaserg <zaz123zaz4@gmail.com>
2022-07-02 17:25:36 +0300

    end

--- app/Http/Middleware/Auth.php
+++ app/Http/Middleware/Auth.php
@@ -3,22 +3,24 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use App\Providers\RouteServiceProvider;
 
 class Auth
 {
     /**
-     * Handle an incoming request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
-     * @return mixed
-     */
+    * Handle an incoming request.
+    *
+    * @param  \Illuminate\Http\Request  $request
+    * @param  \Closure  $next
+    * @return mixed
+    */
     public function handle($request, Closure $next)
     {
-      if (session('auth')) {
-        return $next($request);
-      } else {
-        // redirect
-      }
+        if (session('auth')) {
+            return $next($request);
+        } else {
+            session()->flash('status', 'Для начала введите пароль!');
+            return redirect(RouteServiceProvider::HOME);
+        }
     }
 }
