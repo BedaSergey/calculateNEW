17d3c9fb20157d946e0016c675077c0bad300468
Bedaserg <105390516+Bedaserg@users.noreply.github.com>
2022-07-02 14:16:58 +0300

    Update login.blade.php

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -3,14 +3,20 @@
 @section('title', 'Page Title')
 
 @section('content')
-<div class="p-5 m-5 d-flex flex-column">
-  <form method="POST" action="{{ route('login') }}">
-    @csrf
-    <div class="mt-5 d-flex flex-row">
-      <span class="h-6 p-2">Пароль:</span>
-      <input name="password" class="form-control">
-      <button class="btn btn-success ms-2" type="submit">Войти</button>
+    <div class="p-5 m-5 d-flex flex-column">
+        @if (session('status'))
+            <div class="alert alert-danger" role="alert">
+                {{session('status')}}
+            </div>
+        @endif
+        <form method="POST" action="{{ route('login') }}">
+            @csrf
+            <div class="mt-5 d-flex flex-row">
+                <span class="h-6 p-2">Пароль:</span>
+                <input name="password" class="form-control">
+                <button class="btn btn-success ms-2" type="submit">Войти</button>
+            </div>
+        </form>
     </div>
-  </form>
-</div>
 @endsection
+
